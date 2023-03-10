5e89553044435589e49234d0abc954fb3e0d582e
Love Lyckaro <love.lyckaro@antura.se>
2023-03-10 14:05:42 +0100

    Add example message types

--- Messages.fs
+++ Messages.fs
@@ -1,1 +1,11 @@
-﻿module Redis1.Messages
+﻿module Lyckaro.Messages
+
+open Lyckaro.ExampleDomains
+
+type PersonMessage =
+  | PersonDeleted of string
+  | PersonAdded of string * Person
+  | PersonUpdated of string * Person
+  
+type FamilyMessage =
+  | FamilyAdded of string
