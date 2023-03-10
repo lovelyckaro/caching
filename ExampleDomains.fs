d68b83d81fc72d9ab7787bfb24926c4c03499c48
Love Lyckaro <love.lyckaro@antura.se>
2023-03-10 14:06:38 +0100

    Add bogus generators for example domains

--- ExampleDomains.fs
+++ ExampleDomains.fs
@@ -1,9 +1,18 @@
 ﻿module Lyckaro.ExampleDomains
 
+open Bogus
+
+let f = Faker()
+
 type Person =
   { firstName: string
     lastName: string
     age: uint }
+  
+let generatePerson () =
+  { firstName = f.Name.FirstName()
+    lastName = f.Name.LastName()
+    age = f.Random.Number(18, 60) |> uint }
 
 type DogSpec =
   { owner: Person
@@ -15,5 +24,19 @@ type CatSpec = { age: uint; name: string }
 type Pet =
   | Dog of DogSpec
   | Cat of CatSpec
+  
+let generatePet () =
+  match f.Random.Bool() with
+  | true ->
+      Cat {
+        age = f.Random.UInt(1u, 15u)
+        name = f.Name.FirstName()
+      }
+  | false ->
+      Dog {
+        owner = generatePerson()
+        age = f.Random.UInt(1u, 10u)
+        name = f.Name.FirstName()
+      }
 
 type Family = { people: Person[]; pets: Pet[] }
